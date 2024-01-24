4b78234569b4c9ce487ff3049a17ed878bfc46d0
rotatingcow <ben.arkonovich@gmail.com>
2024-01-24 12:37:10 -0800

    refactor: merge shooter io layer

--- src/main/java/com/frcteam3636/frc2024/subsystems/shooter/ShooterIO.kt
+++ src/main/java/com/frcteam3636/frc2024/subsystems/shooter/ShooterIO.kt
@@ -1,8 +1,14 @@
 package com.frcteam3636.frc2024.subsystems.shooter
 
-import edu.wpi.first.math.geometry.Rotation2d
 import org.littletonrobotics.junction.LogTable
 import org.littletonrobotics.junction.inputs.LoggableInputs
+import com.frcteam3636.frc2024.CANSparkMax
+import com.frcteam3636.frc2024.REVMotorControllerId
+import com.revrobotics.CANSparkLowLevel
+import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.math.util.Units
+import org.littletonrobotics.junction.Logger
+
 
 
 interface ShooterIO {
@@ -29,4 +35,42 @@ interface ShooterIO {
 
     /** Run the launcher flywheels in reverse to intake at the specified percent speed. */
     fun intake(speed: Double) {}
+}
+
+
+class ShooterIOReal : ShooterIO {
+
+    companion object {
+        const val FLYWHEEL_GEAR_RATIO = 1.0
+    }
+
+    private val left = CANSparkMax(REVMotorControllerId.LeftShooterFlywheel, CANSparkLowLevel.MotorType.kBrushless).apply {
+        inverted = true
+        encoder.velocityConversionFactor = Units.rotationsToRadians(1.0) * FLYWHEEL_GEAR_RATIO / 60
+    }
+
+    private val right = CANSparkMax(REVMotorControllerId.RightShooterFlywheel, CANSparkLowLevel.MotorType.kBrushless).apply {
+        encoder.velocityConversionFactor = Units.rotationsToRadians(1.0) * FLYWHEEL_GEAR_RATIO / 60
+        inverted = false
+    }
+
+    override fun updateInputs(inputs: ShooterIO.ShooterIOInputs) {
+        inputs.leftSpeed = Rotation2d(left.encoder.velocity)
+        inputs.rightSpeed = Rotation2d(right.encoder.velocity)
+    }
+
+    override fun shoot(speed: Double, spin: Boolean) {
+        left.set(speed)
+        Logger.recordOutput("Shooter/Left Power", speed)
+        val rSpeed = speed * if (spin) { 0.75 } else { 1.0 }
+        right.set(rSpeed)
+        Logger.recordOutput("Shooter/Right Power", rSpeed)
+    }
+
+    override fun intake(speed: Double) {
+        left.set(-speed)
+        right.set(-speed)
+        Logger.recordOutput("Shooter/Left Power", -speed)
+        Logger.recordOutput("Shooter/Right Power", -speed)
+    }
 }
