4f52f6bd8718967754386a4a57eec0f92b43af93
Max Niederman <max@maxniederman.com>
2024-02-02 19:26:25 -0800

    feat(shooter): add glass mechanism widget

--- src/main/java/com/frcteam3636/frc2024/subsystems/shooter/Shooter.kt
+++ src/main/java/com/frcteam3636/frc2024/subsystems/shooter/Shooter.kt
@@ -1,16 +1,11 @@
 package com.frcteam3636.frc2024.subsystems.shooter
 
-import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC
 import com.frcteam3636.frc2024.Robot
-import com.frcteam3636.frc2024.utils.math.PIDController
-import com.frcteam3636.frc2024.utils.math.PIDGains
-import edu.wpi.first.math.filter.SlewRateLimiter
-import edu.wpi.first.math.geometry.Rotation2d
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
-import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import edu.wpi.first.wpilibj.util.Color8Bit
 import edu.wpi.first.wpilibj2.command.Subsystem
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand
 import org.littletonrobotics.junction.Logger
 
 object Shooter : Subsystem {
@@ -27,11 +22,39 @@ object Shooter : Subsystem {
     }
     private val pivotInputs = PivotIO.Inputs()
 
+    private val mechanism = Mechanism2d(3.0, 3.0, BLACK)
+    private val mechanismRoot = mechanism.getRoot("Shooter", 0.5, 0.5)
+    private val armLigament = mechanismRoot.append(
+        MechanismLigament2d(
+            "Arm", 2.0, 0.0, 10.0, WHITE,
+        )
+    )
+    private val flywheelLigament = armLigament.append(
+        MechanismLigament2d(
+            "Flywheel", 0.25, 0.0, 5.0, BLUE
+        )
+    )
+
+    init {
+        SmartDashboard.putData("Shooter", mechanism)
+    }
+
     override fun periodic() {
         flywheelIO.updateInputs(flywheelInputs)
         Logger.processInputs("Shooter/Flywheels", flywheelInputs)
 
         pivotIO.updateInputs(pivotInputs)
         Logger.processInputs("Shooter/Pivot", pivotInputs)
+
+        armLigament.angle = pivotInputs.position.degrees
+        flywheelLigament.color = if (flywheelInputs.leftSpeed.rotations > 1.0) {
+            BLUE
+        } else {
+            WHITE
+        }
     }
 }
+
+internal val BLACK = Color8Bit("#0a0a0a")
+internal val WHITE = Color8Bit("#ffffff")
+internal val BLUE = Color8Bit("#1d48a3")
