cfd071dc47e88506f4c61e998abf59c0ce5e4603
Max Niederman <max@maxniederman.com>
2024-02-04 01:36:04 -0800

    refactor(coprocessors): make networking config generic

--- coprocessors/hosts/tangerine/default.nix
+++ coprocessors/hosts/tangerine/default.nix
@@ -9,11 +9,9 @@
     networking = {
       hostName = "tangerine";
 
-      interfaces.enp1s0 = {
-        ipv4 = {
-          addresses = [{ address = "10.36.36.10"; prefixLength = 24; }];
-          routes = [{ address = "0.0.0.0"; prefixLength = 0; via = "10.36.36.1"; }];
-        };
+      robotNetwork = {
+        interface = "enp1s0";
+        address = "10.36.36.10";
       };
     };
 
