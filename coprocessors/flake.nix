73877a0d75b31ca754670d4d252732cd86bdcf1a
Gavin-Niederman <gavinniederman@gmail.com>
2024-02-18 17:03:05 -0800

    add: led strip coprocessor config

--- coprocessors/flake.nix
+++ coprocessors/flake.nix
@@ -7,22 +7,36 @@
       url = "github:ryan4yin/nixos-rk3588";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    rgb-2024 = {
+      url = "github:FRC3636/rgb-2024";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-rk3588, }:
-    let
-      inherit (nixpkgs) lib;
-    in
-    rec {
+  outputs = { self, nixpkgs, nixos-rk3588, nixos-hardware, rgb-2024, }:
+    let inherit (nixpkgs) lib;
+    in rec {
       colmena = {
         meta = {
           nixpkgs = import nixpkgs {
             system = "x86_64-linux";
             overlays = [ overlays.default ];
           };
-          specialArgs = {
-            rk3588 = nixos-rk3588.inputs;
+          specialArgs = { rk3588 = nixos-rk3588.inputs; };
+        };
+
+        copepod = {
+          deployment = {
+            targetHost = "10.36.36.20";
+            targetUser = "root";
           };
+
+          imports = [
+            nixos-hardware.nixosModules.raspberry-pi-4
+
+            ./modules
+            ./hosts/copepod
+          ];
         };
 
         tangerine = {
@@ -31,10 +45,7 @@
             targetUser = "root";
           };
 
-          imports = [
-            ./modules
-            ./hosts/tangerine
-          ];
+          imports = [ ./modules ./hosts/tangerine ];
         };
 
         boop = {
@@ -43,9 +54,7 @@
             targetUser = "root";
           };
 
-          nixpkgs = {
-            system = "aarch64-linux";
-          };
+          nixpkgs = { system = "aarch64-linux"; };
 
           imports = [
             nixos-rk3588.nixosModules.orangepi5
@@ -58,24 +67,19 @@
 
       overlays.default = self: super: {
         photonvision = self.callPackage ./pkgs/photonvision.nix { };
+        rgb-2024 = rgb-2024.packages.${self.system}.default;
       };
 
-      devShell = lib.attrsets.genAttrs
-        [
-          "x86_64-linux"
-          "aarch64-linux"
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ]
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          pkgs.mkShell {
-            name = "3636-coprocessors";
-            buildInputs = [
-              pkgs.colmena
-            ];
-          });
+      devShell = lib.attrsets.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ] (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in pkgs.mkShell {
+          name = "3636-coprocessors";
+          buildInputs = [ pkgs.colmena ];
+        });
     };
 }
